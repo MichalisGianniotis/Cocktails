f0b1c4c5e647b7818607f99618211128fc9e91f3
MichalisGianniotis <mixalismixalis001@gmail.com>
2025-11-10 16:18:48 +0200

    10/11

--- app/layout.jsx
+++ app/layout.jsx
@@ -37,6 +37,8 @@ export const metadata = {
   icons: {
     // icon: "data:image/svg+xml,<svg xmlns='http://www.w3.org/2000/svg' viewBox='0 0 100 100'><text y='0.9em' font-size='90'>🍹</text></svg>",
     icon: "data:image/svg+xml,%3Csvg%20xmlns='http://www.w3.org/2000/svg'%20viewBox='0%200%20100%20100'%3E%3Ctext%20y='0.9em'%20font-size='90'%3E%F0%9F%8D%B9%3C/text%3E%3C/svg%3E",
+    shortcut: "data:image/svg+xml,%3Csvg%20xmlns='http://www.w3.org/2000/svg'%20viewBox='0%200%20100%20100'%3E%3Ctext%20y='0.9em'%20font-size='90'%3E%F0%9F%8D%B9%3C/text%3E%3C/svg%3E",
+    apple: "data:image/svg+xml,%3Csvg%20xmlns='http://www.w3.org/2000/svg'%20viewBox='0%200%20100%20100'%3E%3Ctext%20y='0.9em'%20font-size='90'%3E%F0%9F%8D%B9%3C/text%3E%3C/svg%3E",
   }
 };
 
