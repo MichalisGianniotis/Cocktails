c161da2443d250b4b4cba0b2b527433792fe8516
MichalisGianniotis <mixalismixalis001@gmail.com>
2025-11-10 16:25:10 +0200

    10/11

--- app/layout.jsx
+++ app/layout.jsx
@@ -39,7 +39,8 @@ export const metadata = {
     icon: "data:image/svg+xml,%3Csvg%20xmlns='http://www.w3.org/2000/svg'%20viewBox='0%200%20100%20100'%3E%3Ctext%20y='0.9em'%20font-size='90'%3E%F0%9F%8D%B9%3C/text%3E%3C/svg%3E",
     shortcut: "data:image/svg+xml,%3Csvg%20xmlns='http://www.w3.org/2000/svg'%20viewBox='0%200%20100%20100'%3E%3Ctext%20y='0.9em'%20font-size='90'%3E%F0%9F%8D%B9%3C/text%3E%3C/svg%3E",
     apple: "data:image/svg+xml,%3Csvg%20xmlns='http://www.w3.org/2000/svg'%20viewBox='0%200%20100%20100'%3E%3Ctext%20y='0.9em'%20font-size='90'%3E%F0%9F%8D%B9%3C/text%3E%3C/svg%3E",
-  }
+  },
+  manifest: "data:image/svg+xml,%3Csvg%20xmlns='http://www.w3.org/2000/svg'%20viewBox='0%200%20100%20100'%3E%3Ctext%20y='0.9em'%20font-size='90'%3E%F0%9F%8D%B9%3C/text%3E%3C/svg%3E",
 };
 
 export default function RootLayout({ children }) {
