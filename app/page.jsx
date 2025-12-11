dd39b06e10945b4576541bea02cb86b0a0e708bb
MichalisGianniotis <mixalismixalis001@gmail.com>
2025-12-11 18:04:08 +0200

    update 2.1

--- app/page.jsx
+++ app/page.jsx
@@ -23,25 +23,25 @@ export default function CocktailApp() {
   
   
   return (
-    <div className="min-h-screen bg-gray-900 text-white p-10">
+    <div className="min-h-screen w-full bg-gray-900 text-white p-10">
       <h1 className="text-4xl font-bold text-center mb-8">🍹 Cocktail Recipes</h1>
       <div className="max-w-md mx-auto mb-6">
         <Input 
         placeholder="Search cocktails..." 
         onChange={e => setSearch(e.target.value)} 
-        className={'bg-gray-800 text-white border-gray-700 w-full max-w-md'}/>
+        className={'bg-gray-800 text-white border-gray-700 w-full max-w-md '}/>
       </div>
       
       <div className="grid md:grid-cols-3 gap-6">
 
         {displayedCocktails.length === 0 ? (
-            <p className="text-center col-span-full text-lg text-gray-300 italic break-words">
+            <p className="text-center col-span-full text-lg text-gray-300 italic break-all">
             No results found for "<span className="font-bold">{search}</span>"
           </p>
         ) : (
           displayedCocktails.map((cocktail) => (
             // bg-linear-to-br from-black to-green-700
-            <Card key={cocktail.id} className={`text-white shadow-lg rounded-2xl border-2 border-gray-400 mt-6 ${ cocktail.name === "Mojito" ? "bg-linear-to-br from-black to-green-700":
+            <Card key={cocktail.id} className={`text-white shadow-lg rounded-2xl border-3 border-gray-500 mt-6 ${ cocktail.name === "Mojito" ? "bg-linear-to-br from-black to-green-700":
             cocktail.name === "Margarita" ? "bg-linear-to-br from-black to-yellow-300": 
             cocktail.name === "Old Fashioned" ? "bg-linear-to-br from-black to-orange-500" :
             cocktail.name === "Negroni" ? "bg-linear-to-br from-black to-red-600" : ""}`}>
