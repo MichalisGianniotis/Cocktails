ee7f4519dd2aab8a1fc4af5a030859b22741203c
MichalisGianniotis <mixalismixalis001@gmail.com>
2025-12-17 12:41:36 +0200

    Update 2.3.1

--- app/layout.jsx
+++ app/layout.jsx
@@ -31,8 +31,8 @@ export const metadata = {
       images: [
         {
           url: "https://shakermap.vercel.app/cocktails.png",
-          width: 1200,
-          height: 630,
+          // width: 1200,
+          // height: 630,
           alt: "Cocktail Recipes – Mojito, Margarita και άλλα",
         },
       ],
